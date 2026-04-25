81124403ac4e3a9c928f5e681885018785c2107b
Nick Alcock <nick.alcock@oracle.com>
2026-04-25 12:43:20 +0100

    libctf: handle non-bit-width structure members
    libctf returns these with a -1 bit_width (to distinguish them from
    zero-width bitfields), but the rest of pahole wants zero.

--- libctf_loader.c
+++ libctf_loader.c
@@ -238,6 +238,9 @@ static int create_members(struct cu *cu, const struct btf_type *tp, struct type 
 	while ((offset = ctf_member_next(cu->ctf_fp, id, &i, &name, &membtype, &bit_width, 1)) != CTF_ERR) {
 		struct class_member *member = zalloc(sizeof(*member));
 
+		if (bit_width == -1)
+		  bit_width = 0;
+
 		if (member == NULL)
 			return -ENOMEM;
 
